2f79f1ea1f99ff9ea73efc3ac4bf32fd997bfa91
tonatihu <carlostonatihu@gmail.com>
2019-02-14 08:51:16 -0600

    Listo

---
+++ ejer3/EjercicioServlet/src/java/dto/Datos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dto;
+
+/**
+ *
+ * @author tonatihu
+ * Created on 11-Feb-2019
+ */
+public class Datos {
+
+}
