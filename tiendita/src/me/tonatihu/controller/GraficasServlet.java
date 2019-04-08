234aba3036cb439d2feba82c35a45cea2bc3d837
Tonatihu <carlostonatihu@gmail.com>
2019-04-07 20:48:41 -0500

    Ya pude hacer el reporte, cutre pero lo hice

--- tiendita/src/me/tonatihu/controller/GraficasServlet.java
+++ tiendita/src/me/tonatihu/controller/GraficasServlet.java
@@ -3,8 +3,9 @@ package me.tonatihu.controller;
 import me.tonatihu.dao.impl.ProductoDaoImpl;
 import me.tonatihu.dto.Dato;
 import me.tonatihu.util.Paginas;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRLoader;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
@@ -15,10 +16,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 // 1 2 3 4 5
@@ -41,16 +42,35 @@ public class GraficasServlet extends HttpServlet {
 
     private void enviar(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        String ruta = getServletConfig().getServletContext()
-                .getRealPath("/static/template/reporte.jaster");
-        File reporte = new File(ruta, "reporte.pdf");
+        String rutaTemplate = getServletConfig().getServletContext()
+                .getRealPath("/static/templates");
+        String rutaGrafica = getServletConfig().getServletContext()
+                .getRealPath("/static/templates/grafica.png");
+        ProductoDaoImpl dao = new ProductoDaoImpl();
+        List<Dato> datos = dao.getDatos();
         try {
-            JasperFillManager.fillReportToFile(reporte.getAbsolutePath(), new HashMap<>());
-        } catch (JRException e) {
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(new File(rutaTemplate, "tienditaChida.jasper"));
+            JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(datos);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("ds", ds);
+            parameters.put("logo", rutaGrafica);
+            /*
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters,
+                    new JREmptyDataSource());
+            OutputStream out = new FileOutputStream(new File(rutaTemplate, "reporte.pdf"));
+            JasperExportManager.exportReportToPdfStream(jasperPrint, out);
+            */
+            byte[] byteStream = JasperRunManager.runReportToPdf(jasperReport, parameters, new JREmptyDataSource());
+
+            OutputStream outStream = response.getOutputStream();
+            response.setHeader("Content-Disposition","inline, filename=reporte.pdf");
+            response.setContentType("application/pdf");
+            response.setContentLength(byteStream.length);
+            outStream.write(byteStream,0,byteStream.length);
+        } catch (JRException | FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        response.sendRedirect("graficas?accion=ver");
+        //response.sendRedirect("graficas?accion=ver");
     }
 
     private void ver(HttpServletRequest request, HttpServletResponse response)
@@ -82,9 +102,13 @@ public class GraficasServlet extends HttpServlet {
         });
 
         String ruta = getServletConfig().getServletContext().getRealPath("/static/img");
+        String rutaTemplate = getServletConfig().getServletContext()
+                .getRealPath("/static/templates");
         File fileGrafica = new File(ruta, "grafica.png");
+        File graficaTemplate = new File(rutaTemplate, "grafica.png");
         JFreeChart chart = ChartFactory.createPieChart("Productos y Categorias", dpd,
                 true, true, false);
         ChartUtilities.saveChartAsPNG(fileGrafica, chart, 400, 300);
+        ChartUtilities.saveChartAsPNG(graficaTemplate, chart, 400, 300);
     }
 }
